aa99fb2b4e97a4394402f62edf54744475d6f238
Michael Schmid <michael@amazee.com>
2021-04-19 14:50:55 -0400

    - add possibility to skip source downscale - handle non PV namespaces

--- migrate-between-clusters.sh
+++ migrate-between-clusters.sh
@@ -16,6 +16,7 @@ usage() {
   echo "  -n <NAMESPACE>              #required, which namespace should be migrated"
   echo "  -s <SOURCE_CONTEXT>         #required, source context - needs to be a context in your kubeconfig"
   echo "  -d <DESTINATION_CONTEXT>    #required, destination context - needs to be a context in your kubeconfig"
+  echo "  -z skip                     #optional, should the scaledown of source deployments be skipped, by default enabled"
   exit 1
 }
 
@@ -24,7 +25,9 @@ then
   usage
 fi
 
-while getopts ":n:s:d::" opt; do
+SKIP_SOURCE_SCALEDOWN=false
+
+while getopts ":n:s:d:z::" opt; do
   case ${opt} in
     n ) # process option n
       NAMESPACE=$OPTARG;;
@@ -32,6 +35,8 @@ while getopts ":n:s:d::" opt; do
       SOURCE=$OPTARG;;
     d ) # process option d
       DESTINATION=$OPTARG;;
+    z ) # process option z
+      SKIP_SOURCE_SCALEDOWN=$OPTARG;;
     h )
       usage;;
     *)
@@ -45,19 +50,24 @@ fi
 
 # Export all objectes
 # Important: child objects like Pods are not exported as they are created again when the Parent object is created
-echo "configmaps persistentvolumeclaims secrets services deployments.apps horizontalpodautoscalers.autoscaling cronjobs.batch certificates.cert-manager.io ingresses.extensions mariadbconsumers.mariadb.amazee.io mongodbconsumers.mongodb.amazee.io postgresqlconsumers.postgres.amazee.io rolebindings.rbac.authorization.k8s.io roles.rbac.authorization.k8s.io" | xargs -n 1 \
+echo "configmaps persistentvolumeclaims secrets services deployments.apps statefulsets horizontalpodautoscalers.autoscaling poddisruptionbudgets cronjobs.batch certificates.cert-manager.io ingresses.extensions mariadbconsumers.mariadb.amazee.io mongodbconsumers.mongodb.amazee.io postgresqlconsumers.postgres.amazee.io rolebindings.rbac.authorization.k8s.io roles.rbac.authorization.k8s.io" | xargs -n 1 \
   kubectl --context="$SOURCE" get -n "$NAMESPACE" -o json > "$NAMESPACE-original.json"
 
-# Patch PVs to "persistentVolumeReclaimPolicy: Retain" or they could be deleted during the migration
-kubectl --context="$SOURCE" patch pv $(kubectl --context="$DESTINATION" get pv -o=json | jq ".items[]|select(.spec.claimRef.namespace==\"$NAMESPACE\")|.metadata.name" -r) -p '{"spec":{"persistentVolumeReclaimPolicy":"Retain"}}'
-# Export all PVs that are used by the given Namespace
-kubectl --context="$SOURCE" get pv $(kubectl --context="$DESTINATION" get pv -o=json | jq ".items[]|select(.spec.claimRef.namespace==\"$NAMESPACE\")|.metadata.name" -r) -o json > "$NAMESPACE-original-pv.json"
+SOURCE_NAMESPACE_PVS=$(kubectl --context="$DESTINATION" get pv -o=json | jq ".items[]|select(.spec.claimRef.namespace==\"$NAMESPACE\")|.metadata.name" -r)
+if [[ ! -z "$SOURCE_NAMESPACE_PVS" ]]; then
+  # Patch PVs to "persistentVolumeReclaimPolicy: Retain" or they could be deleted during the migration
+  kubectl --context="$SOURCE" patch pv ${SOURCE_NAMESPACE_PVS} -p '{"spec":{"persistentVolumeReclaimPolicy":"Retain"}}'
+  # Export all PVs that are used by the given Namespace
+  kubectl --context="$SOURCE" get pv ${SOURCE_NAMESPACE_PVS} -o json > "$NAMESPACE-original-pv.json"
+fi
 
 # export the Namespace itself, this keeps namespace labels
 kubectl --context="$SOURCE" get ns "$NAMESPACE" -o json > "$NAMESPACE-original-ns.json"
 
-# Scale down all deployments in source namespace, this ensures ReadWriteOnce (RWO) PVs can be mounted by the destination namespace
-kubectl --context="$SOURCE" -n "$NAMESPACE" scale  deployment --all --replicas=0
+if [ "${SKIP_SOURCE_SCALEDOWN}" != "skip" ]; then
+  # Scale down all deployments in source namespace, this ensures ReadWriteOnce (RWO) PVs can be mounted by the destination namespace
+  echo kubectl --context="$SOURCE" -n "$NAMESPACE" scale  deployment --all --replicas=0
+fi
 
 # Create target Namespace
 kubectl --context="$DESTINATION" create -f "$NAMESPACE-original-ns.json"
@@ -67,10 +77,13 @@ kubectl --context="$DESTINATION" -n "$NAMESPACE" create sa lagoon-deployer
 
 # Import our objects
 kubectl --context="$DESTINATION" -n "$NAMESPACE" create -f "$NAMESPACE-original.json"
-kubectl --context="$DESTINATION" -n "$NAMESPACE" create -f "$NAMESPACE-original-pv.json"
+if [[ ! -z "$SOURCE_NAMESPACE_PVS" ]]; then
+  kubectl --context="$DESTINATION" -n "$NAMESPACE" create -f "$NAMESPACE-original-pv.json"
+fi
 
-# Remove the claimref from the PVs as the UID of the PVC have changed, the PVCs are already pointing to the PVs and therefore will be bound immediatelly again
-kubectl --context="$DESTINATION" patch pv $(kubectl --context="$DESTINATION" get pv -o=json | jq ".items[]|select(.spec.claimRef.namespace==\"$NAMESPACE\")|.metadata.name" -r) --type json -p '[{"op": "remove", "path": "/spec/claimRef"}]'
-
-
+DESTINATION_NAMESPACE_PVS=$(kubectl --context="$DESTINATION" get pv -o=json | jq ".items[]|select(.spec.claimRef.namespace==\"$NAMESPACE\")|.metadata.name" -r)
+if [[ ! -z "$DESTINATION_NAMESPACE_PVS" ]]; then
+  # Remove the claimref from the PVs as the UID of the PVC have changed, the PVCs are already pointing to the PVs and therefore will be bound immediatelly again
+  kubectl --context="$DESTINATION" patch pv ${DESTINATION_NAMESPACE_PVS} --type json -p '[{"op": "remove", "path": "/spec/claimRef"}]'
+fi
 
